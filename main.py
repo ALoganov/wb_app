2acef397b870ad94236e3f0d290b555eac4abbb2
Alexey Loganov <n0x.alexey@gmail.com>
2026-04-24 17:28:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def get_wb_stats():
         rev_with_disc = sum(i.get('priceWithDisc', 0) for i in yesterday_orders)
         rev_finished = sum(i.get('finishedPrice', 0) for i in yesterday_orders)
         rev_total = sum(i.get('totalPrice', 0) for i in yesterday_orders)
+        rev_disc = sum(i.get('discount', 0) for i in yesterday_orders)
         
         return {
             "status": "success",
@@ -61,7 +62,8 @@ def get_wb_stats():
                 "debug_sums": {
                     "if_priceWithDisc": int(rev_with_disc),
                     "if_finishedPrice": int(rev_finished),
-                    "if_totalPrice": int(rev_total)
+                    "if_totalPrice": int(rev_total),
+                    "discount": int(rev_disc/5)
                 }
             }
         }
